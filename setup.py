5c83d15d53e6a173fb84f74e8bebbc7b9991c319
avollkopf <43980694+avollkopf@users.noreply.github.com>
2021-02-25 18:50:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='cbpi4-BM_PID_SmartBoilWithPump',
       version='0.0.1',
       description='CraftBeerPi Plugin',
@@ -12,4 +18,6 @@ setup(name='cbpi4-BM_PID_SmartBoilWithPump',
       '': ['*.txt', '*.rst', '*.yaml'],
       'cbpi4-BM_PID_SmartBoilWithPump': ['*','*.txt', '*.rst', '*.yaml']},
       packages=['cbpi4-BM_PID_SmartBoilWithPump'],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
      )
